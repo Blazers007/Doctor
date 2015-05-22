cb6a11f68f61050df569c02a0095868e6aed4f2a
Blazers <blazers_d_ar@outlook.com>
2015-05-22 16:43:45 +0800

    Add Wave View

--- app/src/main/java/com/blazers/app/doctor/Tools/RoundWaveView.java
+++ app/src/main/java/com/blazers/app/doctor/Tools/RoundWaveView.java
@@ -1,15 +1,14 @@
 package com.blazers.app.doctor.Tools;
 
 import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.RectF;
+import android.graphics.*;
 import android.os.Handler;
 import android.os.Message;
 import android.util.AttributeSet;
 import android.view.View;
 import com.blazers.app.doctor.R;
+
+import java.util.Random;
 
 /**
  * Created by liang on 2015/5/21.
@@ -19,6 +18,9 @@ public class RoundWaveView extends View {
     private Paint circlePaint;
     private Paint roundPaint;
     private Paint wavePaint;
+    /* Wave Path */
+    private Path wavePath;
+    private Path wavePath2;
 
     public RoundWaveView(Context context) {
         super(context);
@@ -73,6 +75,10 @@ public class RoundWaveView extends View {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
+            if(getMeasuredWidth() != 0)
+                fai = (fai + 5) % getMeasuredWidth();
+            else
+                fai += 5;
             invalidate();
             sendEmptyMessageDelayed(0, 16);
         }
@@ -94,28 +100,65 @@ public class RoundWaveView extends View {
 
         circlePaint = new Paint();
         circlePaint.setAntiAlias(true);
-        circlePaint.setAlpha(64);
         circlePaint.setStyle(Paint.Style.STROKE);
         circlePaint.setStrokeWidth(10);
         circlePaint.setColor(getResources().getColor(R.color.white));
+        circlePaint.setAlpha(64);
 
         wavePaint = new Paint();
         wavePaint.setAntiAlias(true);
-        wavePaint.setAlpha(192);
         wavePaint.setColor(Color.WHITE);
+        wavePaint.setAlpha(4);
+
+        /* Init Path */
+        wavePath = new Path();
+
+        handler.sendEmptyMessage(0);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         int centerX = getMeasuredWidth() / 2;
         int centerY = getMeasuredHeight() / 2;
-        int radius = Math.min(centerX, centerY);
+        int radius = Math.min(centerX, centerY)- 20; /* 缩小一个padding */
 
         canvas.drawCircle(centerX, centerY, radius, roundPaint);
-
         RectF rectF = new RectF(centerX - radius, centerY - radius, centerX + radius, centerY + radius);
-        canvas.drawArc(rectF, 0, 359, false, circlePaint);
-
+        canvas.drawArc(rectF, 0, 360, true, roundPaint);
         /* Wave */
+        setPath();
+        canvas.save();
+        Path round = new Path();
+        round.addCircle(centerX, centerY, radius, Path.Direction.CCW);
+        canvas.clipPath(round, Region.Op.REPLACE);
+        canvas.drawPath(wavePath, wavePaint);
+        canvas.restore();
+
+    }
+
+    // 左右偏移 φ
+    private int fai = 0;
+    // 上下偏移
+    private float k = 200;
+    // 角速度
+    private float w = 0.7f;
+    // 振幅
+    private int a = 20;
+
+    void setPath() {
+        int x = 0;
+        int y = 0;
+        wavePath.reset();
+        for(int i = 0 ; i < getMeasuredWidth() ; i ++) {
+            x = i;
+            y = (int) (a * Math.sin((i * w + fai) * Math.PI / 180) + k);
+            if (i == 0) {
+                wavePath.moveTo(x, y);
+            }
+            wavePath.quadTo(x, y, x + 1, y);
+        }
+        wavePath.lineTo(getMeasuredWidth(), getMeasuredHeight());
+        wavePath.lineTo(0, getMeasuredHeight());
+        wavePath.close();
     }
 }
