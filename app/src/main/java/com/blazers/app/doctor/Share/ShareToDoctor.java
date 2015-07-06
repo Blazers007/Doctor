be838e0f5ebfdf2c51cbe9070cc10edde5e2ce11
Blazers <308802880@qq.com>
2015-07-06 08:46:29 +0800

    Try  to merge two branches step2

--- app/src/main/java/com/blazers/app/doctor/Share/ShareToDoctor.java
+++
@@ -1,9 +0,0 @@
-package com.blazers.app.doctor.Share;
-
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by liang on 2015/5/29.
- */
-public class ShareToDoctor extends AppCompatActivity {
-}
